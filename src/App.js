13859157960d3466bc296df538016130a104a53b
Matt Hackmann <mhackmann13@icloud.com>
2021-12-21 13:48:52 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <ul id="tabs">
+          <li><span className="selected">Today</span></li>
+          <li>All time</li>
+        </ul>
+        <div id="main-header">
+          <div id="main-header-calories">
+            <div>
+              <span id="main-header-calories-consumed">1395</span> of <span id="main-header-calories-goal">3100</span> calories
+            </div>
+            <div id="main-header-progress-bar">
+              <div></div>
+            </div>
+          </div>
+          <div id="main-header-macros-consumed">
+            <ul>
+              <li>Fat: 0 g</li>
+              <li>Carbs: 0 g</li>
+              <li>Protein: 0 g</li>
+            </ul>
+          </div>
+          <div id="main-header-weigh-in">
+              <span id="main-header-weigh-in-count">165 <span>lbs</span></span>
+              <span>Today's weight</span>
+          </div>
+          <div id="main-header-miles-ran">
+            <span id="main-header-miles-count">3.65 <span>Miles</span></span> 
+            <span>Travelled today</span>
+          </div>
+          <div id="main-header-weight-lifted">
+            <span id="main-header-weight-count">13,465 <span>lbs</span></span> 
+            <span>Lifted today</span>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+      </div>
     </div>
   );
 }
