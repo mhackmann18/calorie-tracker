d722e0fff44f99f9d4fab36e172ee043908c9653
Matt Hackmann <mhackmann13@icloud.com>
2021-12-27 15:55:52 -0600

    Added HTML for the food page's main content

--- src/App.js
+++ src/App.js
@@ -18,19 +18,41 @@ function App() {
           <button>Runs</button>
           <button>Workouts</button>
         </div>
-        <div id="nutrition-main">
-          <div id="nutrition-main-left">
-            <div id="nutrition-main-left-upper">
 
+        <div id="food-tracking-page">
+          <div id="food-tracking-main">
+            <div id="food-tracking-main-row-1">
+              <h2>Food Tracking</h2>
+              <div id="food-tracking-main-change-date">
+                <i class="fas fa-caret-left"></i>
+                <span id="food-tracking-main-date">Today, Sun Dec 26</span>
+                <i class="fas fa-caret-right"></i>
+              </div>
             </div>
-            <div id="nutrition-main-left-lower">
-              
+            <div id="food-tracking-main-row-2">
+              <div id="food-tracking-main-search-box">
+                <input type="text" />
+                <i class="fas fa-search"></i>
+              </div>
+              or
+              <button id="food-tracking-main-add-food-btn">Add a food manually</button>
+            </div>
+            <h3>Breakfast</h3>
+            <div className="food-item">
+              <div className="food-item-text">
+                <h5>Food 1</h5>
+                <p>Serving Size: 1 cup</p>
+              </div>
+              <div className="food-item-btns">
+              <i class="fas fa-plus"></i>
+              </div>
             </div>
           </div>
-          <div id="nutrition-main-right">
+          <div id="food-tracking-cards">
             
           </div>
         </div>
+
       </div>
     </div>
   );
