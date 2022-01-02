c4417a237a8130667e176052ecf9c23d04570026
Matt Hackmann <mhackmann13@icloud.com>
2022-01-02 10:54:44 -0600

    Getting things up to date

--- src/pages/foods/DateChange.js
+++ src/pages/foods/DateChange.js
@@ -1,11 +1,26 @@
 import './DateChange.css';
+import { useState } from 'react';
 
 function DateChange(){
+  const [date, setDate] = useState(new Date());
+
+  console.log(date);
+
+  let options = { weekday: "long", month: "short", day: "numeric" };
+  
+  function addDay(){
+    setDate(date.getDate() + 1);
+  }
+
+  function subtractDay(){
+    setDate(date.getDate() - 1);
+  }
+
   return (
     <div id="food-tracking-main-change-date">
-      <i className="fas fa-caret-left fa-lg btn"></i>
-      <span id="food-tracking-main-date">Today, Sun Dec 26</span>
-      <i className="fas fa-caret-right fa-lg btn"></i>
+      <i className="fas fa-caret-left fa-lg btn" onClick={subtractDay}></i>
+      <span id="food-tracking-main-date">{date.toDateString()  ? "Today, " : ""}{date.toLocaleDateString("en-US", options)}</span>
+      <i className="fas fa-caret-right fa-lg btn" onClick={addDay}></i>
     </div>
   );
 }
