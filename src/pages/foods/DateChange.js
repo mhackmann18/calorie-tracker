bafd273eb9f70f80d2be33a6160a2839ed83dc9c
Matt Hackmann <mhackmann13@icloud.com>
2022-01-02 12:20:47 -0600

    Prettied up DateChange component

--- src/pages/foods/DateChange.js
+++ src/pages/foods/DateChange.js
@@ -4,13 +4,13 @@ function DateChange(props){
 
   let options = { weekday: "long", month: "short", day: "numeric" };
   
-  function handleRightClick(){
+  function addDay(){
     let dateCopy = new Date(props.date);
     dateCopy.setDate(dateCopy.getDate() + 1);
     props.onDateChange(dateCopy);
   }
 
-  function handleLeftClick(){
+  function subtractDay(){
     let dateCopy = new Date(props.date);
     dateCopy.setDate(dateCopy.getDate() - 1);
     props.onDateChange(dateCopy);
@@ -18,9 +18,12 @@ function DateChange(props){
 
   return (
     <div id="food-tracking-main-change-date">
-      <i className="fas fa-caret-left fa-lg btn" onClick={handleLeftClick}>&lt;</i>
-      <span id="food-tracking-main-date">{(new Date(props.date)).toDateString() === (new Date()).toDateString()  ? "Today, " : ""}{(new Date(props.date)).toLocaleDateString("en-US", options)}</span>
-      <i className="fas fa-caret-right fa-lg btn" onClick={handleRightClick}>&gt;</i>
+      <i className="fas fa-caret-left fa-lg btn" onClick={subtractDay}>&lt;</i>
+      <span>
+        {(new Date(props.date)).toDateString() === (new Date()).toDateString() ? "Today, " : ""}
+        {(new Date(props.date)).toLocaleDateString("en-US", options)}
+      </span>
+      <i className="fas fa-caret-right fa-lg btn" onClick={addDay}>&gt;</i>
     </div>
   );
 }
