2e070c7764b8cfd2f33047de2f1cce50ceaec1d3
Matt Hackmann <mhackmann13@icloud.com>
2022-01-09 09:32:13 -0600

    Removed isDateToday util function

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,6 @@ export function ISOStringToDate(string){
   return new Date(string.replace(/-/g, '/').replace(/T.+/, ''));
 }
 
-export function areStringsSameDate(string1, string2){
+export function areDateStringsSameDate(string1, string2){
   return ISOStringToDate(string1).toDateString() === ISOStringToDate(string2).toDateString();
 } 
-
-export function isDateStringToday(string){
-  return ISOStringToDate(string).toDateString() === (new Date()).toDateString();
-}
