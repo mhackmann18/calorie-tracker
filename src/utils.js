f0123445db4db2cdb2727aff231663edc4397fa4
Matt Hackmann <mhackmann13@icloud.com>
2022-01-09 09:27:32 -0600

    Added new util function for checking if date string is today

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,7 @@ export function ISOStringToDate(string){
 export function areStringsSameDate(string1, string2){
   return ISOStringToDate(string1).toDateString() === ISOStringToDate(string2).toDateString();
 } 
+
+export function isDateStringToday(string){
+  return ISOStringToDate(string).toDateString() === (new Date()).toDateString();
+}
