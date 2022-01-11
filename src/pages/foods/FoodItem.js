813b7ceed9ec529ff1731d35ea83058baf0e718d
Matt Hackmann <mhackmann13@icloud.com>
2022-01-10 21:47:16 -0600

    Added fa icons to FoodItem

--- src/pages/foods/FoodItem.js
+++ src/pages/foods/FoodItem.js
@@ -1,14 +1,30 @@
 import './FoodItem.css';
 
-function FoodItem(){
+function FoodItem(props){
+  const { name, servingSize, quantity, calories } = props.data;
+
+  function handleDeleteClick(){
+    console.log("Delete food");
+  }
+
+  function handleEditClick(){
+    console.log("Edit servings");
+  }
+
   return (
     <div className="food-item">
-      <div className="food-item-text">
-        <h5>Food 1</h5>
-        <p>Serving Size: 1 cup | Calories: 150</p>
+      <div className="food-item-main">
+        <div className="food-item-text">
+          <h4>{name}</h4>
+          <p>Serving Size: {servingSize} | Servings: {quantity} | Calories: {calories}</p>
+        </div>
+        <div className="food-item-btns">
+          <i onClick={handleEditClick} className="far fa-edit btn" title="Edit servings"></i>
+          <i onClick={handleDeleteClick} className="far fa-trash-alt btn" title="Remove"></i>
+        </div>
       </div>
-      <div className="food-item-btns">
-        <i className="fas fa-plus btn"></i>
+      <div className="food-item-dropdown">
+        
       </div>
     </div>
   );
